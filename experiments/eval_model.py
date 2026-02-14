bef20f2d9a19443ca5945f0583bc31e41e49d5c9
andreea-ghe <gheorghe.andreea.ioana26@gmail.com>
2026-02-14 16:13:25 +0200

    refactor: switch from wandb to csv

--- experiments/eval_model.py
+++ experiments/eval_model.py
@@ -50,7 +50,6 @@ def test_model(config):
         logger_name = f"{config.MODEL_NAME}_{config.LOG_FILE_NAME}"
     else:
         logger_name = f"{config.MODEL_NAME}_{NOW_TIME}"
-
     logger = CSVLogger(
         save_dir=model_save_path,
         name=logger_name,
