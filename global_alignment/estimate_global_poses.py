88bc86a237e4304dfa8987f5c0b6d250c9cfc77a
andreea-ghe <gheorghe.andreea.ioana26@gmail.com>
2026-02-28 11:42:08 +0200

    debug: remove message

--- global_alignment/estimate_global_poses.py
+++ global_alignment/estimate_global_poses.py
@@ -96,8 +96,6 @@ def global_transform(pred_match_matrix, part_pcs, n_valid, n_pcs, critical_point
                 target_pcd = o3d.geometry.PointCloud()
                 target_pcd.points = o3d.utility.Vector3dVector(critical_target_points)
 
-                print("THIS CASE WITH MANY CORRESPONDENCES")
-
                 refined_rigid_transform = o3d.pipelines.registration.registration_icp(
                     source=source_pcd,
                     target=target_pcd,
@@ -175,7 +173,6 @@ def global_transform(pred_match_matrix, part_pcs, n_valid, n_pcs, critical_point
                 edges.append(np.array([idx2, idx1]))
                 rigid_transform = np.eye(4)
                 matchin1, matching2 = np.nonzero(match_submatrix)
-                print("THIS CASE WITH FEW CORRESPONDENCES")
                 rigid_transform[:3, 3] = np.mean(critical_target_points[matching2], axis=0) - np.mean(critical_source_points[matchin1], axis=0)
                 
                 source_pcd = o3d.geometry.PointCloud()
