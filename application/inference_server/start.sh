5eaa01ff95991964c1756df539a76391ea1fc53e
andreea-ghe <gheorghe.andreea.ioana26@gmail.com>
2026-05-03 15:27:49 +0300

    feat: fix start script

--- application/inference_server/start.sh
+++ application/inference_server/start.sh
@@ -19,7 +19,7 @@ echo "[OK] Conda environment 'assembly' activated"
 # install inference-server-specific dependencies
 echo ""
 echo "[1/2] Installing inference server dependencies..."
-pip install -r "requirements.txt"
+pip install -r "$SCRIPT_DIR/requirements.txt"
 
 # start the FastAPI server
 HOST="${HOST:-0.0.0.0}"
